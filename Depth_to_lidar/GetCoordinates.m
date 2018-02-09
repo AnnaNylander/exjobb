ebd34874861d8d6fb4c79a91bfdd8c827a6ff043
AnnaNylander <35011945+AnnaNylander@users.noreply.github.com>
2018-02-10 00:26:29 +0100

    Solved final corrections for lidar. Lidar point-cloud is no longer warped.

--- Depth_to_lidar/GetCoordinates.m
+++ Depth_to_lidar/GetCoordinates.m
@@ -2,22 +2,19 @@ function [x,y,z] = GetCoordinates(imageA,angles, pixels)
     pixels = uint32(pixels);
     nPoints = length(pixels);
     c = zeros(nPoints,3);
-    
+
     %for i = 1:nVertical
        for i = 1:nPoints
            v = degtorad(angles(i,1));
            h = degtorad(angles(i,2));
            r = imageA(pixels(i,3),pixels(i,1));
-           
+
            [x,y,z] = sph2cart(h,v,r);
            
-           %c(pointIndex,1) = (1/cos(h))*x;
-           %c(pointIndex,2) = (1/cos(h))*y;
-           %c(pointIndex,3) = z;
-           
-           c(i,1) = -(1/cos(pi - h))*x; %-h
-           c(i,2) = (1/sin(pi/2 - h))*y; %pi/2 -h
-           c(i,3) = z;
+           %final correction.
+           c(i,1) = (1/cos(h))*(1/cos(v))*x;
+           c(i,2) = (1/cos(h))*(1/cos(v))*y;
+           c(i,3) = (1/cos(h))*(1/cos(v))*z;
        end
     %end
 
@@ -25,4 +22,3 @@ function [x,y,z] = GetCoordinates(imageA,angles, pixels)
     y = c(:,2);
     z = c(:,3);
 end
-
