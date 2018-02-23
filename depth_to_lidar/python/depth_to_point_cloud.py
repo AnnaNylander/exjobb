8d3d331dd3da10f06ca1f1a7608933e753212cb7
Jacob Genander <genander@student.chalmers.se>
2018-02-23 14:59:03 +0100

    Add Python implementation of corrected interpolation

--- depth_to_lidar/python/depth_to_point_cloud.py
+++ depth_to_lidar/python/depth_to_point_cloud.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import misc
+from interpolate_2D import interpolate_2D
 
 def rotate_z(points,theta):
     """Rotate an array of points theta degrees around the z-axis."""
@@ -21,11 +22,8 @@ def depth_to_point_cloud(imgHead, imgTail, imgLeft, imgRight, far, interpolate=F
     x_coords, y_coords = coords[:,0], coords[:,1]
 
     n_points = len(x_coords)
-    vHead = np.zeros([n_points,1])
-    vTail = np.zeros([n_points,1])
-    vLeft = np.zeros([n_points,1])
-    vRight = np.zeros([n_points,1])
 
+    # Interpolate or get raw pixel values
     if interpolate:
         vHead = interpolate_2D(head, x_coords, y_coords, threshold)
         vTail = interpolate_2D(tail, x_coords, y_coords, threshold)
@@ -52,88 +50,11 @@ def depth_to_point_cloud(imgHead, imgTail, imgLeft, imgRight, far, interpolate=F
     pointCloud = np.concatenate((cHead,cTail,cLeft,cRight),0)
     return pointCloud
 
-def interpolate_2D(values,x_query,y_query,threshold):
-    """Conditioned interpolation between pixel values in the queried points.
-
-    Let q be the pixel that the query point lies in. If the difference in pixel
-    value between q and the three neighboring pixels closest to the query point
-    is larger than threshold, then no interpolation is performed; the value of q
-    is used."""
-
-    # Four pixels with indices cX and cY:
-    # (cX1,cY1) (cX2,cY1)
-    # (cX1,cY2) (cX2,cY2)
-
-    n_points = len(x_query)
-    interpolatedValues = np.zeros([n_points,1]);
-
-    # For each point
-    for i in range(n_points):
-
-        # Instantiate coordinate variables
-        cX1, cX2, cY1, cY2 = 0, 0, 0, 0
-        fx, fy = 0, 0
-
-        # Get the query point coordinates
-        Xq = x_query[i] - 1
-        Yq = y_query[i] - 1
-
-        # Get horizontal pixel index on which the point lies
-        cX = Xq.astype(int)
-
-        # decimal part of point from pixel's beginning
-        decimalX = Xq - cX
-
-        # If the queried point is to the left of the pixel's center
-        if decimalX <= 0.5:
-            cX1 = cX # Set cX1 to the be the pixel index of cX
-            cX2 = cX - 1 # We're interested in the pixel to the left of cX
-            fx = decimalX + 0.5
-        else:
-            cX1 = cX + 1 # % We're interested in the pixel to the right of cX
-            cX2 = cX # Set cX2 to the be the pixel index of cX
-            fx = decimalX - 0.5
-
-        # Repeat procedure for y-axis
-        cY = Yq.astype(int)
-        decimalY = Yq - cY
-
-        if decimalY <= 0.5:
-            cY1 = cY
-            cY2 = cY - 1
-            fy = decimalY + 0.5
-        else:
-            cY1 = cY + 1
-            cY2 = cY
-            fy = decimalY - 0.5
-
-        # Get value in each pixel of relevance
-        v1, v2, v3, v4 = values[cY1,cX1], values[cY1,cX2], values[cY2,cX1], values[cY2,cX2]
-
-        # Calculate difference in pixel intensity between pixel hit by
-        # query point and the other three pixels.
-       	v = values[cY,cX]
-        d1, d2, d3, d4 = abs(v - v1), abs(v - v2), abs(v - v3), abs(v - v4)
-
-        c = 0
-        #If difference is larger than threshold, do not interpolate.
-        if max(d1, d2, d3, d4) > threshold:
-            c = v
-        else:
-            # Linearly interpolate between pixel values to find value in query point
-            a = v1*fx + v2*(1-fx)
-            b = v3*fx + v4*(1-fx)
-            c = a*fy + b*(1-fy)
-
-        interpolatedValues[i] = c
-    return interpolatedValues
-
 def get_pixel_values(values, x_coords, y_coords):
-    x_coords = x_coords.astype(int) - 1 # Compensate for matlab indexing
-    y_coords = y_coords.astype(int) - 1
+    x_coords = x_coords.astype(int)
+    y_coords = np.ceil(y_coords).astype(int) - 1
 
     n_points = len(x_coords)
-    print(np.max(y_coords))
     pixel_values = np.zeros([n_points, 1])
 
     for i in range(n_points):
@@ -167,7 +88,7 @@ def get_coordinates(values, angles):
 
     for i in range(len(angles)-1):
            v = np.radians(angles[i,0])
-           h = -np.radians(angles[i,1])
+           h = np.radians(angles[i,1])
            r = values[i]
 
            # Correction for differences between lidar and depthmaps measurements
--- depth_to_lidar/python/main.py
+++ depth_to_lidar/python/main.py
@@ -4,34 +4,35 @@ from depth_to_point_cloud import depth_to_point_cloud
 from point_cloud_to_image import trim_to_roi
 from PIL import Image
 
-roi = 60        # Region of interest side length, in meters.
-cells = 600     # Number of cells on a side in the output image
-far = 1000      # Far plane distance
+ROI = 60        # Region of interest side length, in meters.
+CELLS = 600     # Number of cells on a side in the output image
+FAR = 1000      # Far plane distance
+THRESHOLD = 1.5
 
 # Read depth maps
-head = misc.imread('../example_images/head/image_00034.png')
-tail = misc.imread('../example_images/tail/image_00034.png')
-left = misc.imread('../example_images/left/image_00034.png')
-right = misc.imread('../example_images/right/image_00034.png')
+head = misc.imread('../example_images/head/image_00060.png')
+tail = misc.imread('../example_images/tail/image_00060.png')
+left = misc.imread('../example_images/left/image_00060.png')
+right = misc.imread('../example_images/right/image_00060.png')
 
 # Convert depth maps to 3D point cloud
-point_cloud = depth_to_point_cloud(head, tail, left, right, far, interpolate=True, threshold=1.0)
+point_cloud = depth_to_point_cloud(head, tail, left, right, FAR, interpolate=True, threshold=THRESHOLD)
 # Trim point cloud to only contain points within the region of interest
-point_cloud = trim_to_roi(point_cloud,roi)
+point_cloud = trim_to_roi(point_cloud,ROI)
 # TODO Count number of points within each grid cell
 # TODO Save as image
 
-grid = np.zeros([cells,cells])
-print(point_cloud.shape)
+grid = np.zeros([CELLS,CELLS])
+
 for point in point_cloud:
     x, y, z = point
-    x += roi/2
-    x /= roi
-    cell_x = int(x*cells)
+    x += ROI/2
+    x /= ROI
+    cell_x = int(x*CELLS)
 
-    y += roi/2
-    y /= roi
-    cell_y = int(y*cells)
+    y += ROI/2
+    y /= ROI
+    cell_y = int(y*CELLS)
 
     grid[cell_x,cell_y] += 1
 
