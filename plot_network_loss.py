d997b045d7a1094929377ea80a24cba3e10d0772
Anna Nylander <anna.s.nylander@gmail.com>
2018-04-18 16:17:41 +0200

    also save fig when plot loss

--- plot_network_loss.py
+++ plot_network_loss.py
@@ -6,10 +6,14 @@ parser = argparse.ArgumentParser(description='Plot training progress')
 parser.add_argument('-m','--model', metavar='folder', type=str,
                     dest='path', default='',
                     help='Foldername to model where the loss files are stored. Eg. \'LucaNet/\' (with trailing /)')
+parser.add_argument('-s','--save_name', metavar='name', type=str,
+                    dest='name', default='',
+                    help='Name of figure when saved. (default: <epochs trained>_<min_val_loss>.png )')
 parser.add_argument('--ylim', metavar='N', type=int,
 			        dest='ylim', default=100, help='Y-axis positive limit')
 parser.add_argument('-a','--average', metavar='N', type=int,
 			        dest='avg', default=100, help='Moving average window')
+
 
 args = parser.parse_args()
 PATH_BASE = '/media/annaochjacob/crucial/models/'
@@ -35,9 +39,15 @@ def main():
     train_handle = plt.plot(train[:,0], train_values, label="Training loss")
     validate_handle = plt.plot(validate[:,0], validate_values, label="Validation loss")
 
+    filename = 'batchTrained=' + str(len(train_values)) + '_valLoss=' + str(min(validate_values)) + '_ylim=' + str(args.ylim)
+
+    if args.name:
+        filename=args.name
+
     axes = plt.gca()
     axes.set_xlim([0,np.max(train[:,0])])
     axes.set_ylim([0,args.ylim])
+    plt.savefig(PATH_BASE + args.path + filename + '.png')
     plt.show()
 
 if __name__ == "__main__":
