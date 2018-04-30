069fb4c2c5180864232f1c00b7dc8b263039d630
Anna Nylander <anna.s.nylander@gmail.com>
2018-04-30 16:12:15 +0200

    Update train script

--- train.sh
+++ train.sh
@@ -9,16 +9,16 @@ FOLDER_NAME=trained_2_epochs/
 DATASET=eukaryote/
 
 # Recurrent network
-#python3 main.py -e $EPOCHS -b 1 -pl $PLOT_FREQ -p $PRINT_FREQ -a rnn.LSTMNet -pf 29 -fs 1 -d $DATASET -s ${FOLDER_NAME}LSTMNet/ -o "SGD( model.parameters(), lr=3*1e-4, momentum=0.9, weight_decay=1e-5)" --scheduler
-#python3 main.py -e $EPOCHS -b 1 -pl $PLOT_FREQ -p $PRINT_FREQ -a rnn.LSTMNetBi -pf 29 -fs 1 -d $DATASET -s ${FOLDER_NAME}LSTMNetBi/ -o "SGD( model.parameters(), lr=3*1e-4, momentum=0.9, weight_decay=1e-5)" --scheduler
-#python3 main.py -e $EPOCHS -b 1 -pl $PLOT_FREQ -p $PRINT_FREQ -a rnn.GRUNet -pf 29 -fs 1 -d $DATASET -s ${FOLDER_NAME}GRUNet/ -o "SGD( model.parameters(), lr=1e-5, momentum=0.9, weight_decay=1e-5)" --scheduler
+python3 main.py -e $EPOCHS -b 1 -pl $PLOT_FREQ -p $PRINT_FREQ -a rnn.LSTMNet -pf 29 -fs 1 -d $DATASET -s ${FOLDER_NAME}LSTMNet/ -o "SGD( model.parameters(), lr=3*1e-4, momentum=0.9, weight_decay=1e-5)" --scheduler
+python3 main.py -e $EPOCHS -b 1 -pl $PLOT_FREQ -p $PRINT_FREQ -a rnn.LSTMNetBi -pf 29 -fs 1 -d $DATASET -s ${FOLDER_NAME}LSTMNetBi/ -o "SGD( model.parameters(), lr=3*1e-4, momentum=0.9, weight_decay=1e-5)" --scheduler
+python3 main.py -e $EPOCHS -b 1 -pl $PLOT_FREQ -p $PRINT_FREQ -a rnn.GRUNet -pf 29 -fs 1 -d $DATASET -s ${FOLDER_NAME}GRUNet/ -o "SGD( model.parameters(), lr=1e-5, momentum=0.9, weight_decay=1e-5)" --scheduler
 
-#python3 main.py -e $EPOCHS -b $BATCH_SIZE -pl $PLOT_FREQ -p $PRINT_FREQ -a cnn_deluxe.CNNOnly -d $DATASET -s ${FOLDER_NAME}CNNOnly/ -bl -o "SGD( model.parameters(), lr=3*1e-4, momentum=0.9, weight_decay=1e-5)" --scheduler
-#python3 main.py -e $EPOCHS -b 8 -pl $PLOT_FREQ -p $PRINT_FREQ -a cnn_deluxe.CNNLSTM -d $DATASET -s ${FOLDER_NAME}CNNLSTM/ -bl -o "SGD( model.parameters(), lr=3*1e-4, momentum=0.9, weight_decay=1e-5)" --scheduler
-#python3 main.py -e $EPOCHS -b $BATCH_SIZE -pl $PLOT_FREQ -p $PRINT_FREQ -a network.SmallerNetwork1 -d $DATASET -s ${FOLDER_NAME}SmallerNetwork1/ -bl -o "SGD( model.parameters(), lr=1e-5.5, momentum=0.9, weight_decay=1e-5)" --scheduler
-#python3 main.py -e $EPOCHS -b $BATCH_SIZE -pl $PLOT_FREQ -p $PRINT_FREQ -a network.SmallerNetwork2 -d $DATASET -s ${FOLDER_NAME}SmallerNetwork2/ -bl -o "SGD( model.parameters(), lr=1e-5, momentum=0.9, weight_decay=1e-5)" --scheduler
-#python3 main.py -e $EPOCHS -b $BATCH_SIZE -pl $PLOT_FREQ -p $PRINT_FREQ -a cnn_bias.CNNBiasFirst -d $DATASET -s ${FOLDER_NAME}CNNBiasFirst/ -bl -o "SGD( model.parameters(), lr=1e-5, momentum=0.9, weight_decay=1e-5)" --scheduler
-#python3 main.py -e $EPOCHS -b $BATCH_SIZE -pl $PLOT_FREQ -p $PRINT_FREQ -a cnn_bias.CNNBiasAll -d $DATASET -s ${FOLDER_NAME}CNNBiasAll/ -bl -o "SGD( model.parameters(), lr=1e-5.75, momentum=0.9, weight_decay=1e-5)" --scheduler
+python3 main.py -e $EPOCHS -b $BATCH_SIZE -pl $PLOT_FREQ -p $PRINT_FREQ -a cnn_deluxe.CNNOnly -d $DATASET -s ${FOLDER_NAME}CNNOnly/ -bl -o "SGD( model.parameters(), lr=3*1e-4, momentum=0.9, weight_decay=1e-5)" --scheduler
+python3 main.py -e $EPOCHS -b 8 -pl $PLOT_FREQ -p $PRINT_FREQ -a cnn_deluxe.CNNLSTM -d $DATASET -s ${FOLDER_NAME}CNNLSTM/ -bl -o "SGD( model.parameters(), lr=3*1e-4, momentum=0.9, weight_decay=1e-5)" --scheduler
+python3 main.py -e $EPOCHS -b $BATCH_SIZE -pl $PLOT_FREQ -p $PRINT_FREQ -a network.SmallerNetwork1 -d $DATASET -s ${FOLDER_NAME}SmallerNetwork1/ -bl -o "SGD( model.parameters(), lr=1e-5.5, momentum=0.9, weight_decay=1e-5)" --scheduler
+python3 main.py -e $EPOCHS -b $BATCH_SIZE -pl $PLOT_FREQ -p $PRINT_FREQ -a network.SmallerNetwork2 -d $DATASET -s ${FOLDER_NAME}SmallerNetwork2/ -bl -o "SGD( model.parameters(), lr=1e-5, momentum=0.9, weight_decay=1e-5)" --scheduler
+python3 main.py -e $EPOCHS -b $BATCH_SIZE -pl $PLOT_FREQ -p $PRINT_FREQ -a cnn_bias.CNNBiasFirst -d $DATASET -s ${FOLDER_NAME}CNNBiasFirst/ -bl -o "SGD( model.parameters(), lr=1e-5, momentum=0.9, weight_decay=1e-5)" --scheduler
+python3 main.py -e $EPOCHS -b $BATCH_SIZE -pl $PLOT_FREQ -p $PRINT_FREQ -a cnn_bias.CNNBiasAll -d $DATASET -s ${FOLDER_NAME}CNNBiasAll/ -bl -o "SGD( model.parameters(), lr=1e-5.75, momentum=0.9, weight_decay=1e-5)" --scheduler
 python3 main.py -e $EPOCHS -b $BATCH_SIZE -pl $PLOT_FREQ -p $PRINT_FREQ -a cnn_bias.CNNBiasLast -d $DATASET -s ${FOLDER_NAME}CNNBiasLast/ -bl -o "SGD( model.parameters(), lr=1e-5.75, momentum=0.9, weight_decay=1e-5)" --scheduler
 
 
