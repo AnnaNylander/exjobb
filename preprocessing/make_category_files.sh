3694d6baa0701fc1fc6cdd219f8af486ef3e3b2e
Anna Nylander <anna.s.nylander@gmail.com>
2018-05-28 13:43:52 +0200

    Add category file maker script

---
+++ preprocessing/make_category_files.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cd ~/Repos/exjobb/preprocessing/
+
+NAME=mango
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=banana
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=watermelon
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=kiwi
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=strawberry
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=carambola
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=litchi
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=rambutan
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=pitahaya
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=durian
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=apple
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=orange
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=pear
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=raspberry
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=papaya
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=pineapple
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=peach
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=jackfruit
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=lemon
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
+NAME=pomegranate
+python3.6 categorize_dataset.py -a train/${NAME}/categories/${NAME}.csv -s train/${NAME}/categories/categories.csv
